d0ff5f069be36867e0455df901fe3d8edd93e50e
Dekguh <kadekteguhmahesa@gmail.com>
2021-08-16 12:24:30 +0800

    splash container

---
+++ src/components/container/SplashContainer.js
@@ -0,0 +1,27 @@
+import { withStyles } from '@ui-kitten/components'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SplashContainer = ({ eva, style, ...restProps }) => {
+    return (
+        <View style={eva.style.container}>
+            <Text style={eva.style.title}>WASSME.</Text>
+        </View>
+    )
+}
+
+export const ThemedSplashContainerView = withStyles(SplashContainer, (theme) => ({
+    container: {
+        padding: 20,
+        backgroundColor: theme['color-primary-500'],
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        color: '#FFFFFF',
+        fontSize: 21,
+        fontWeight: 'bold',
+        fontStyle: 'italic'
+    }
+}))
