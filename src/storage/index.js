dde3f452835d69acdd0f25282d037bb38cb0a4a3
Dekguh <kadekteguhmahesa@gmail.com>
2021-08-16 12:22:31 +0800

    jt storage

---
+++ src/storage/index.js
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export async function setJwtStorage(jwt) {
+    try {
+        await AsyncStorage.setItem('@jwt', jwt)
+    } catch(err) {
+        return null
+    }
+}
+
+export async function removeJwtStorage() {
+    try {
+        await AsyncStorage.removeItem('@jwt')
+    } catch(err) {
+        return null
+    }
+}
+
+export async function getJwtStorage() {
+    try {
+        const jwt = await AsyncStorage.getItem('@jwt')
+        return jwt
+    } catch(err) {
+        return null
+    }
+}
