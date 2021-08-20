ac68352c904a6c848699eb37380045ccb3d4ef46
Dekguh <kadekteguhmahesa@gmail.com>
2021-08-20 16:24:18 +0800

    create screen for bottom tab

---
+++ src/components/screens/LogoutScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import SplashContainer from '../container/SplashContainer'
+
+const LogoutScreen = () => {
+    return (
+    <>
+        <SplashContainer />
+    </>
+    )
+}
+
+export default LogoutScreen
