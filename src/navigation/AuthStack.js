4facccfb75912acf312aad349c88e573482173bb
Dekguh <kadekteguhmahesa@gmail.com>
2021-08-26 18:23:46 +0800

    remove registerScreen

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LoginScreen from '../components/screens/LoginScreen'
-import RegisterScreen from '../components/screens/RegisterScreen'
 
 const Stack = createNativeStackNavigator()
 
