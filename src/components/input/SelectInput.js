dbdd3e9976d8211846ddb14738561f929091581e
Dekguh <kadekteguhmahesa@gmail.com>
2021-08-29 20:23:21 +0800

    update getValue props

--- src/components/input/SelectInput.js
+++ src/components/input/SelectInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { IndexPath, Select, SelectItem } from '@ui-kitten/components';
 
-export const SelectInput = ({ listSelect, ...props }) => {
+export const SelectInput = ({ listSelect, getValue, ...props }) => {
     const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
 
     return (
@@ -11,7 +11,10 @@ export const SelectInput = ({ listSelect, ...props }) => {
             placeholder='all'
             value={listSelect[selectedIndex.row].title}
             selectedIndex={selectedIndex}
-            onSelect={index => setSelectedIndex(index)}>
+            onSelect={index => {
+                setSelectedIndex(index)
+                getValue(listSelect[index.row].title)
+            }}>
             {listSelect && listSelect.map((data, i) => (
                 <SelectItem title={data.title} key={i} />
             ))}
